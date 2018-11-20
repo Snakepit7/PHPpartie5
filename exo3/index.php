97a81551b9d1fac0fbe6d8c1553880279b6ca23f
Snakepit7 <julienfollet71@gmail.com>
2018-11-20 10:33:32 +0100

    exos

---
+++ exo3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo3</title>
+  </head>
+  <body>
+      <?php
+        $months = array ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+         
+        echo $months[4] . '<br />';
+        
+      ?>
+  </body>
+</html>
---
+++ exo4/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo4</title>
+  </head>
+  <body>
+      <?php
+        $months = array ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+         
+        $months[7]='Août';
+        
+        echo $months[7];
+        
+      ?>
+  </body>
+</html>
