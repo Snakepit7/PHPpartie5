402efa0eb5ea878e2304fbbc23511e3d740baa04
Snakepit7 <julienfollet71@gmail.com>
2018-11-20 10:48:30 +0100

    exos

---
+++ exo5/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo5</title>
+  </head>
+  <body>
+      <?php
+        $departements = array (
+                            02 => 'Aisne',
+                            59 => 'Nord',
+                            60 => 'Oise',
+                            62 => 'Pas-de-Calais',
+                            80 => 'Somme',
+        )
+      ?>
+  </body>
+</html>
