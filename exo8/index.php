2069559c40224db8e05bab3a023a9b229892e1c8
Snakepit7 <julienfollet71@gmail.com>
2018-11-20 11:09:03 +0100

    exos

---
+++ exo8/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo8</title>
+  </head>
+  <body>
+      <?php
+        $months = array ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+         
+        for ($numero = 0; $numero < 11; $numero++) {
+
+        echo $months[$numero] . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
+
+        }
+      ?>
+  </body>
+</html>
