3e20c1226f01ad4476f46c6393228112692957c8
Snakepit7 <julienfollet71@gmail.com>
2018-11-20 12:01:24 +0100

    exos

--- exo8/index.php
+++ exo8/index.php
@@ -1,18 +1,32 @@
 <!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Exo8</title>
-  </head>
-  <body>
-      <?php
-        $months = array ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
-         
-        for ($numero = 0; $numero < 11; $numero++) {
-
-        echo $months[$numero] . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
-
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Exo8</title>
+    </head>
+    <body>
+        <?php
+        //Création du tableau
+        $monthsArray = array(
+            'Janvier',
+            'Février',
+            'Mars',
+            'Avril',
+            'Mai',
+            'Juin',
+            'Juillet',
+            'Août',
+            'Septembre',
+            'Octobre',
+            'Novembre',
+            'Décembre'
+        );
+        //Boucle foreach qui permet d'afficher le contenu du tableau
+        //On va chercher $month dans le tableau $monthArray, puis afficher le résultat avec un echo
+        foreach ($monthsArray as $month) {
+            echo $month;
+            ?><br /><?php
         }
-      ?>
-  </body>
+        ?>
+    </body>
 </html>
